2143966c3d2d644d3f071cf4e560b96aeacaabc3
Aiman Haziq <aimanhaziqyazik@gmail.com>
2023-03-30 10:40:29 +0800

    added copyright to footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -127,7 +127,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} MyCardiacRehab, build with passion and love.`,
       },
       prism: {
         theme: lightCodeTheme,
